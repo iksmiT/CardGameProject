6861d008c104ab0ca7f3f032549ef1970538e213
iksmiT <tim.vongsana@hotmail.fr>
2016-03-02 23:18:50 +0100

    Initial commit

---
+++ app/src/main/java/timski_ensicaen/cardgameproject/MainActivity.java
@@ -0,0 +1,15 @@
+package timski_ensicaen.cardgameproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
