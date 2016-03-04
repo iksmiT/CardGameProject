c1a71b9b4273534938b9bc64687c67c569d035af
Quentin <quentin.simard@hotmail.fr>
2016-03-04 23:00:35 +0100

    commit

--- app/src/main/java/timski_ensicaen/cardgameproject/MainActivity.java
+++ app/src/main/java/timski_ensicaen/cardgameproject/MainActivity.java
@@ -2,6 +2,8 @@ package timski_ensicaen.cardgameproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void PlayGame(View view){
+        Intent intent = new Intent(this,null);
+        startActivity(intent);
+    }
+
 
 }
+
+
---
+++ app/src/main/java/timski_ensicaen/cardgameproject/PlayGame.java
@@ -0,0 +1,32 @@
+package timski_ensicaen.cardgameproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.content.Intent;
+import android.view.View;
+
+public class PlayGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        setContentView(R.layout.activity_play_game);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
