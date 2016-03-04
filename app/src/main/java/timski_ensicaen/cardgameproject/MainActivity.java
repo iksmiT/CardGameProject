3036204e8d489ca87c6fb583eb85f2faf3793d72
iksmiT <tim.vongsana@hotmail.fr>
2016-03-04 23:03:26 +0100

    Settings App

--- app/src/main/java/timski_ensicaen/cardgameproject/MainActivity.java
+++ app/src/main/java/timski_ensicaen/cardgameproject/MainActivity.java
@@ -1,15 +1,40 @@
 package timski_ensicaen.cardgameproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "Hommage à notre enseignant Gilles Lebrun.";
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        final EditText underTitle = (EditText) findViewById(R.id.underTitle);
+        String message = underTitle.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        final Button playButton = (Button) findViewById(R.id.play);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+            }
+        });
+        */
     }
-
-
 }
+
+
+
