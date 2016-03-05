15145aa1b6a9f9f8c6c68592b061b681c48d9ac6
iksmiT <tim.vongsana@hotmail.fr>
2016-03-05 11:40:07 +0100

    Blank activity when clicking on PLAY button

--- app/src/main/java/timski_ensicaen/cardgameproject/MainActivity.java
+++ app/src/main/java/timski_ensicaen/cardgameproject/MainActivity.java
@@ -1,7 +1,9 @@
 package timski_ensicaen.cardgameproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void playButton(View view){
+        Intent intent = new Intent(this, CardGameActivity.class);
+        startActivity(intent);
+    }
 }
